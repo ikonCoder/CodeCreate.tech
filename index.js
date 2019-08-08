9285b0d243946fe865a8e80582ef9f730cbed1d1
Matthew Joseph <matthew.j.joe@gmail.com>
2019-08-08 18:51:35 -0400

    Added some jQuery to give a hide/show function to services section

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $("h1").click(function(){
+        $(".serviceImg-container").animate({right: '250px'});
+        $(".serviceImg-container").show(4100);
+      });  
+      
+      
+
+  });
