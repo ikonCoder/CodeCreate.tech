4304d4cbe3ebf1ff409344a74f1ef5aa6091d635
Matthew Joseph <matthew.j.joe@gmail.com>
2019-08-13 02:04:56 -0400

    Completed my contact me page - still need to figure out how to get the data posted from the froms into a database ... but the front end is complete. :w

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-$(document).ready(function(){
-
-    $("h1").click(function(){
-        $(".serviceImg-container").animate({right: '250px'});
-        $(".serviceImg-container").show(4100);
-      });  
-      
-      
-
-  });
+  $(document).ready(function(){
+    $(".serviceImg-container").animate({right: '250px'});
+    $(".serviceImg-container").show(2000, "linear");
+}
+);
