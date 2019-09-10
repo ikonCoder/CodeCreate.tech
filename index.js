a47918ae56c9d413100f8b6e74aa8096f18c5ca8
Matthew Joseph <matthew.j.joe@gmail.com>
2019-09-10 19:29:31 -0400

    This is adding some js that allow me to have a modal that pops up when a user wants to know more information about CodeCreates services

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
   // JQUERY
 
-
-
-  $(document).scroll(function(){
-    $(".serviceImg-container").animate({right: '300px'});
-    $(".serviceImg-container").show(2000, "linear");
-});
+//   $(document).scroll(function(){
+//     $(".serviceImg-container").animate({right: '300px'});
+//     $(".serviceImg-container").show(2000, "linear");
+// });
 
 
 //JavaScript
-
-
 
 //Form Validation 
 
@@ -32,21 +28,42 @@ function Validation()
 
 }
 
-//Shows prices for services packages
- 
-const bluredImage = document.getElementsByClassName("service-image");
-const routeBtn = document.getElementsByClassName("serviceDetailText");
-routeBtn.addEventListener("click", blurImage2);
- 
+
+// Get the modal
+var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var service1 = document.getElementById("Btn1");
+var service2 = document.getElementById("Btn2")
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close1")[0];
+var span2 = document.getElementsByClassName("close2")[0];
 
 
-function blurImage2(){
-  if(true){
-    bluredImage.className = "blured-image";
-  } 
-  else{
-    return("null");
-  }
+// When the user clicks on the button, open the modal
+service1.onclick = function() {
+  modal1.style.display = "block";
+}
+service2.onclick = function() {
+  modal2.style.display = "block";
 }
 
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
