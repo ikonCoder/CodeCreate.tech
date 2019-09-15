10c024e80a015441aa0be3a74bcc81f0cf84942b
Matthew Joseph <matthew.j.joe@gmail.com>
2019-09-15 00:36:34 -0400

    Testing new external API for form valid.

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
   // JQUERY
 
-  $(document).scroll(function(){
-    $(".serviceImg-container").animate({right: '300px'});
-    $(".serviceImg-container").show(2000, "linear");
+$(document).scroll(function(){
+  $(".serviceImg-container").animate({right: '300px'});
+  $(".serviceImg-container").show(2000, "linear");
 });
+
+$(document).ready(function () {
+  $('.submit').click(function (event){
+    console.log("Clicked Button") 
+
+    var name = $('.name').val()
+    var email = $('.email').val()
+    var subject = $('.subject').val()
+    var message = $('.message').val()
+    var statusElm = $('.status')
+    statusElm.empty()
+
+    if(name.length > 1) {
+      statusElm.append('<div>Name is valid</div>')
+    } else{
+      event.preventDefault()
+      statusElm.append('<div>Name is not valid</div>')
+    }
+
+    if(email.length > 5 && email.includes('@') && email.indludes('.')) {
+      statusElm.append('<div>Email is valid</div>')
+    } else{
+      event.preventDefault()
+      statusElm.append('<div>Email is not valid</div>')
+    }
+
+    if(subject.length > 2) {
+      statusElm.append('<div>Subject is valid</div>')
+    } else{
+      event.preventDefault()
+      statusElm.append('<div>Subject is not valid</div>')
+    }
+
+    if(message.length > 20) {
+      statusElm.append('<div>Message is valid</div>')
+    } else{
+      event.preventDefault()
+      statusElm.append('<div>Message is not valid</div>')
+    }
+  })
+})
 
 
 //JavaScript
-
-//Form Validation 
-
-function Validation()
-{
-  let firstName = document.getElementById("firstName");
-  let lastName = document.getElementById("lastName");
-  let msg = document.getElementById("message");
-
-
-  if(firstName.value == "" || lastName.value == "" || msg.value == "")
-  {
-  alert("humm.. looks like you are missing some information!");
-  }
-
-  else{
-    alert("Awesome, your message has been sent!");
-  }
-
-}
-
 
 // Get the modal
 var modal1 = document.getElementById("myModal1");
@@ -67,3 +87,6 @@ window.onclick = function(event) {
     modal2.style.display = "none";
   }
 }
+
+
+
