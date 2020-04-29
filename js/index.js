ed55d49d2a0bdb1650715f6ef994807788bf3b0b
unknown <matthew.j.joe@gmil.com>
2020-04-29 04:28:39 -0400

    desktop styles are done, need to add php emailer

--- js/index.js
+++ js/index.js
@@ -65,32 +65,11 @@ var span1 = document.getElementsByClassName("close1")[0];
 var span2 = document.getElementsByClassName("close2")[0];
 
 
-// When the user clicks on the button, open the modal
-service1.onclick = function() {
-  modal1.style.display = "block";
-}
-service2.onclick = function() {
-  modal2.style.display = "block";
-}
 
-// When the user clicks on <span> (x), close the modal
-span1.onclick = function() {
-  modal1.style.display = "none";
-}
-span2.onclick = function() {
-  modal2.style.display = "none";
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal1) {
-    modal1.style.display = "none";
-  }
-  if (event.target == modal2) {
-    modal2.style.display = "none";
-  }
-}
-
+const scroll = new SmoothScroll('.navbarr a[href*="#"]', {
+	speed: 500
+});
 
 
 
